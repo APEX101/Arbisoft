21d6048b887e1ab848a623d8848b033e2467c614
APEX101 <ahmadhassan159@hotmail.com>
2021-08-11 16:34:48 +0500

    fix

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from parsing_class import FileParsing
+from calculation_class import QueryCalculations
+from plotting_class import PlotGraph
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print("This is my first pycharm program")
-    
+# Returning concated dataframes for the given year
+df = FileParsing(year="2016")
+df = df.perform_parsing()
+# print(df)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Returning Calculated results for the given query
+calculate_high_temp = QueryCalculations(df, query="high_temp")
+calculate_high_temp = calculate_high_temp.perform_calculations_yearly()
+print((calculate_high_temp))
+
+calculate_low_temp = QueryCalculations(df, query="low_temp")
+calculate_low_temp = calculate_low_temp.perform_calculations_yearly()
+print((calculate_low_temp))
+#
+calculate_humidity = QueryCalculations(df, query="most_humidity")
+calculate_humidity = calculate_humidity.perform_calculations_yearly()
+print((calculate_humidity))
+
+# Monthly Queries
+avg_high_temp = QueryCalculations(df, query="avg_high_temp")
+avg_high_temp = avg_high_temp.perform_calculations_monthly(month=6)
+print("Average highest temperature for given month is {}".format(avg_high_temp))
+
+avg_low_temp = QueryCalculations(df, query="avg_low_temp")
+avg_low_temp = avg_low_temp.perform_calculations_monthly(month=6)
+print("Average lowest temperature for given month is {}".format(avg_low_temp))
+
+avg_humidity = QueryCalculations(df, query="avg_humidity")
+avg_humidity = avg_humidity.perform_calculations_monthly(month=6)
+print("Average humidity for given month is {}".format(avg_humidity))
+
+
+# Plotting For Given year and given month
+drawing = PlotGraph(dataframe=df, month=5, charttype="normal")
+drawing = drawing.perform_plotting()
+print(drawing)
+
+drawing2 = PlotGraph(dataframe=df, month=5, charttype="stacked")
+drawing2 = drawing2.perform_plotting()
+print(drawing2)
